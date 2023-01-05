b6efa61ac20bc0f67d485b65883a75ed9a162ca6
dezilim <63087319+dezilim@users.noreply.github.com>
2023-01-05 14:06:18 +0800

    Add files via upload

---
+++ CameraFollow.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    float timeOffset;
+
+    [SerializeField] 
+    Vector2 posOffset;
+
+    [SerializeField]
+    float leftLimit;
+
+    [SerializeField]
+    float rightLimit; 
+
+
+    [SerializeField]
+    float bottomLimit;
+
+    [SerializeField]
+    float topLimit; 
+    public Vector3 velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 startPos = transform.position;
+        Vector3 endPos = player.transform.position;
+
+        endPos.x += posOffset.x;
+        endPos.y += posOffset.y;
+        endPos.z = -10;
+
+        transform.position = Vector3.Lerp(startPos, endPos, timeOffset * Time.deltaTime);
+
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+            transform.position.z
+        );
+
+
+
+        
+    }
+}
